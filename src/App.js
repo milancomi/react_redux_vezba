20e58d3bbd02e35b5095161f188ba94c5ee61493
milancomi <milancomi96@gmail.com>
2019-11-12 02:56:11 +0100

    sdfs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import store from './redux/store'
 import {Provider} from 'react-redux'
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/iceCreamContainer';
 
 function App() {
   return (
     <Provider store ={store}>
     <div className="App">
+      {/* <HooksCakeContainer/>
+       */}
+       <IceCreamContainer/>
       <CakeContainer />
     </div>
     </Provider>
